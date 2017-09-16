9e332690e92550608f7469bf6fdc416b84f2252c
Luiz Eduardo R Figueiredo Paglioni <luizeduard0@iMac-de-Luiz.local>
2017-09-16 20:05:07 -0300

    refactor: Implementing Listbooks and Bookshelf Refactor
    Bookshelf is responsible for displaying different shelfs (i.e: Currently Reading, Read, Want to Read, etc...)
    ListBooks is a higher component that has multiple BookShelfs.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import SearchBooks from './SearchBooks'
-import BookShelf from './BookShelf'
+import ListBooks from './ListBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -14,14 +14,17 @@ class BooksApp extends React.Component {
   componentDidMount() {
     BooksAPI
       .getAll()
-      .then(books => this.setState({ books }))
+      .then(books => {
+        this.setState({ books })
+        console.log('BOOKS', books)
+      })
 
   }
 
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={BookShelf} />
+        <Route exact path="/" component={ListBooks} />
         <Route exact path="/search" component={SearchBooks} />
       </div>
     )
---
+++ src/ListBooks.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import BookShelf from './BookShelf'
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf />
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBooks
