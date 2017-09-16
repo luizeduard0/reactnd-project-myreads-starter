f195f00beaf8425d11ce541de72eb006e844171c
Luiz Eduardo R Figueiredo Paglioni <luizeduard0@iMac-de-Luiz.local>
2017-09-16 20:43:45 -0300

    refactor: Better Book Image Display code

--- src/Book.js
+++ src/Book.js
@@ -13,7 +13,7 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+book.imageLinks.smallThumbnail+'")' }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
           <div className="book-shelf-changer">
             <select>
               <option value="none" disabled>Move to...</option>
