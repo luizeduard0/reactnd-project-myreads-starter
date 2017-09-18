2068e62c25f91c7be26de9c002ec3842dfde0a53
Luiz Eduardo R Figueiredo Paglioni <luizeduard0@iMac-de-Luiz.local>
2017-09-18 20:19:25 -0300

    refactor: Loading component returns <span /> when not loading

--- src/Loading.js
+++ src/Loading.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Loading = props => {
-  return props.show ? <div className="loader"><div>Loading...</div></div> : null
+  return props.show ? <div className="loader"><div>Loading...</div></div> : <span />
 }
 
 export default Loading
